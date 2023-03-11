8cefd67162cec72a898a0d43d1d1c70677dbc825
yimikao <holayinkajr@gmail.com>
2023-03-11 22:34:47 +0100

    basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+
+    let _query = &args[1];
+    let filename = &args[2];
+
+    let file_contents = fs::read_to_string(filename).expect("file could not be read");
+
+    println!("{}", file_contents);
 }
